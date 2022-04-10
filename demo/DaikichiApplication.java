fa303e19131e184969279b203be9d483ccebe96b
Zaragoza2123 <zaragoza.coyote618@gmail.com>
2022-04-10 17:59:13 -0500

    a

--- demo/DaikichiApplication.java
+++ demo/DaikichiApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 //2. Importing dependencies
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PathVariable;
 @SpringBootApplication
 
 //1. annotation 
@@ -29,6 +30,18 @@ public class DaikichiApplication {
     @RequestMapping("/daikichi/tomorrow")
     public String tomorrow() {
     	return "Tomorrow, an opportunity will arise, so be sure to be open to new ideas!";
+    }
+    @RequestMapping("daikichi/travel/{city}")
+    public String vaca(@PathVariable("city") String city) {
+    	return "Congratulations! You will soon travel to "+city+"!";
+    }
+    @RequestMapping("daikichi/lotto/{num}")
+    public String lotto(@PathVariable("num") int num) {
+    	if (num % 2 == 0) {
+    		return "You will take a grand journey in the near future, but be weary of tempting offers.";
+    	} else {
+    		return "You have enjoyed the fruits of your labor but now is a great time to spend time with family and friends.";
+    	}
     }
 		
 }
